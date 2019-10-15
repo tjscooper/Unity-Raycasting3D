d44d521aae22fa98971276edb5761f89ab8d486a
Tim Cooper <tim@Tims-MacBook-Pro.local>
2019-10-15 19:42:41 -0400

    Add cubes and hit detection with ray

---
+++ Assets/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+
+        // The out parameter will write to the hitInfo variable from inside the method
+        // The Physics.Raycast method returns a bool
+        if (Physics.Raycast(ray, out hitInfo, 100))
+        {
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+            print(hitInfo.transform.gameObject.tag);
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
+      
+    }
+}
